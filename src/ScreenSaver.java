07e7aef5ef9d7771cced4d397ad0e966c3d13401
Kletsun Yuri <klec@speroteck.com>
2014-05-20 16:45:42 +0300

    Working copy Dots move from randomize position.

--- src/ScreenSaver.java
+++ src/ScreenSaver.java
@@ -5,91 +5,177 @@ import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
+import java.awt.geom.Line2D;
+import java.awt.geom.Point2D;
+import java.awt.geom.Rectangle2D;
 
-public class ScreenSaver extends JFrame implements ActionListener
+public class ScreenSaver extends JPanel implements ActionListener
 {
-    private MyPoint[] mypoints;
+    private MyPoint[] mypoints = new MyPoint[10];
 
     // no-argument constructor
     public ScreenSaver()
     {
-        this.getContentPane().setBackground(Color.BLACK);
-//        this.setUndecorated(true);
-        this.setResizable(false);
-        this.setSize(300,400);
-        this.setVisible(true);
-//        GraphicsEnvironment.getLocalGraphicsEnvironment()
-//        .getDefaultScreenDevice()
-//        .setFullScreenWindow(this);
+        setOpaque(true);
+        this.setBackground(Color.BLACK);
     }
 
     public void addPoints(){
-        mypoints = new MyPoint[10];
-        for(int i=0; i<10; i++) {
+        GraphicsDevice monitor = GraphicsEnvironment.getLocalGraphicsEnvironment().getDefaultScreenDevice();
+        this.setSize(monitor.getDisplayMode().getWidth(), monitor.getDisplayMode().getHeight());
+        mypoints = new MyPoint[100];
+        for(int i=0; i<100; i++) {
             mypoints[i] = new MyPoint(getSize());
-            this.getContentPane().add(mypoints[i]);
         }
         createScreenUpdate();
     }
 
-    // create and position GUI components; register event handlers
+    public void paint(Graphics g){
+        super.paintComponent(g);
+        Graphics2D g2 = (Graphics2D) g;
+        RenderingHints rh = new RenderingHints(RenderingHints.KEY_ANTIALIASING,
+        RenderingHints.VALUE_ANTIALIAS_ON);
+
+        rh.put(RenderingHints.KEY_RENDERING, RenderingHints.VALUE_RENDER_QUALITY);
+
+        g2.setRenderingHints(rh);
+
+        for(int j=0; j<100; j++){
+            mypoints[j].render(g2);
+        }
+
+    }
+
     private void createScreenUpdate()
     {
-        Timer autoUpdate = new Timer(100, this );
+        Timer autoUpdate = new Timer(30, this );
         autoUpdate.start();
 
-    } // end method createScreenUpdate
+    }
 
     // main method
     public static void main( String[] args )
     {
+        JFrame frame = new JFrame("TimerBasedAnimation");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+//        frame.setSize(300,400);
         ScreenSaver application = new ScreenSaver();
+        frame.add(application);
         application.addPoints();
-        application.setDefaultCloseOperation( JFrame.EXIT_ON_CLOSE );
+//        frame.setVisible(true);
 
-    } // end method main
+        frame.setUndecorated(true);
+        frame.setResizable(false);
+        GraphicsEnvironment.getLocalGraphicsEnvironment()
+        .getDefaultScreenDevice()
+        .setFullScreenWindow(frame);
+
+
+        MouseListener ml = new MouseListener() {
+            @Override
+            public void mouseClicked(MouseEvent mouseEvent) {
+                System.exit(0);
+            }
+
+            @Override
+            public void mousePressed(MouseEvent mouseEvent) { }
+
+            @Override
+            public void mouseReleased(MouseEvent mouseEvent) {}
+
+            @Override
+            public void mouseEntered(MouseEvent mouseEvent) {}
+
+            @Override
+            public void mouseExited(MouseEvent mouseEvent) {}
+        };
+
+        frame.addMouseListener(ml);
+    }
 
     @Override
     public void actionPerformed(ActionEvent e) {
-          this.repaint();
-//        for(int j=0; j<10; j++){
-//            mypoints[j].repaint();
-//        }
+          repaint();
     }
 
 } // end class ScreenSaver
 
-class MyPoint extends JComponent {
+class MyPoint extends Line2D {
 
-    private int x,y,vx,vy;
+    private double x,y,vx,vy;
+    private Color c;
+
     private Dimension ssize;
     public MyPoint(Dimension size) {
-        setSize(1,1);
         int x = size.width;
         int y = size.height;
         ssize=size;
-        this.x=(int) (Math.random()*300);
-        this.y=(int) (Math.random()*400);
-        vx=0;
-        vy=0;
+        this.x= Math.random()*x;
+        this.y= Math.random()*y;
+        this.vx=0;
+        this.vy=0;
+        this.c = new Color((float)Math.random(), (float)Math.random(), (float)Math.random());
     }
 
-    public void paint(Graphics g) {
+    public void render(Graphics2D g) {
+
         if(this.x>ssize.width/2)
-            vx--;
+            this.vx--;
         else
-            vx++;
+            this.vx++;
 
         if(this.y>ssize.height/2)
-            vy--;
+            this.vy--;
         else
-            vy++;
+            this.vy++;
         //setLocation(getX()+1, getY());
-        this.x=this.x+vx;
-        this.y=this.y+vy;
+        this.x=this.x+this.vx;
+        this.y=this.y+this.vy;
         //System.out.println(x+"_"+vx+"_"+ssize.width/2);
-        g.setColor(Color.BLUE);
-        g.drawLine(this.x, this.y, this.x+vx, this.y+vy);
+        g.setColor(this.c);
+        g.draw(this);
 
+    }
+
+    @Override
+    public double getX1() {
+        return x;
+    }
+
+    @Override
+    public double getY1() {
+        return y;
+    }
+
+    @Override
+    public Point2D getP1() {
+        return null;
+    }
+
+    @Override
+    public double getX2() {
+        return x+vx;
+    }
+
+    @Override
+    public double getY2() {
+        return y+vy;
+    }
+
+    @Override
+    public Point2D getP2() {
+        return null;
+    }
+
+    @Override
+    public void setLine(double v, double v2, double v3, double v4) {
+        //this.gr.dra
+    }
+
+    @Override
+    public Rectangle2D getBounds2D() {
+        return null;
     }
 }
